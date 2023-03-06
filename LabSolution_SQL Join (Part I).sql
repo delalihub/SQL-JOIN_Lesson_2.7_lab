652ccaa99096e5de3f06a84fef935191c780ecd2
Delali Ansah <ansah.delaligmail.com>
2023-03-06 10:28:52 +0100

    re addding SQL query lab. 2.07

--- LabSolution_SQL Join (Part I).sql
+++ LabSolution_SQL Join (Part I).sql
@@ -28,4 +28,51 @@ USING (actor_id)
 GROUP BY a.actor_id
 ORDER BY count(f.film_id) DESC;
 
+#4.Most active customer (the customer that has rented the most number of films)
+SELECT * FROM sakila.customer;
+SELECT * FROM sakila.rental;
+
+SELECT c.customer_id, count(*)  FROM sakila.customer c
+JOIN sakila.rental r
+ON c.customer_id = r.customer_id
+GROUP BY c.customer_id
+ORDER BY count(*) DESC
+LIMIT 1;
+
+#.5. Display the first and last names, as well as the address, of each staff member.
+SELECT * FROM Sakila.staff;
+SELECT * FROM sakila.address;
+SELECT concat(first_name, ' ', last_name), address FROM sakila.staff s
+JOIN sakila.address a
+ON s.address_id = a.address_id;
+#alternatively
+SELECT first_name, last_name, address FROM sakila.staff s
+JOIN sakila.address a
+ON s.address_id = a.address_id;
+
+#.6.List each film and the number of actors who are listed for that film.
+SELECT * FROM sakila.film;
+SELECT * FROM sakila.film_actor;
+
+SELECT f.film_id, count(actor_id) FROM sakila.film f
+JOIN sakila.film_actor fa
+ON f.film_id = fa.film_id
+GROUP BY f.film_id
+ORDER BY count(actor_id) DESC;
+
+-- Using the tables payment and customer and the JOIN command, list the total paid.. 
+-- by each customer. List the customers alphabetically by last name.
+SELECT * FROM sakila.payment;
+SELECT * FROM sakila.customer;
+
+SELECT concat(c.last_name, '      ', first_name) AS customer_name, sum(p.amount) 
+ FROM sakila.payment p 
+JOIN sakila.customer c
+ON p.customer_id = c.customer_id
+GROUP BY customer_name
+ORDER BY customer_name ASC;
+
+#8.same as question1(List number of films per category)
+
+
 
